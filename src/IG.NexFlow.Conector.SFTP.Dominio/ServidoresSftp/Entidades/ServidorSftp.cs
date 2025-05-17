baf0f19ffba896d1d807c6967a755f42372849e7
Igor Daflon <igordafloncouto@gmail.com>
2025-05-16 23:07:31 -0300

    Reestruturação do projeto

--- src/IG.NexFlow.Conector.SFTP.Dominio/ServidoresSftp/Entidades/ServidorSftp.cs
+++
@@ -1,8 +0,0 @@
-﻿using IG.NexFlow.Core.Dominio.Entidades;
-
-namespace IG.NexFlow.Conector.SFTP.Dominio.ServidoresSftp.Entidades
-{
-    public class ServidorSftp : EntidadeAuditadaBase
-    {
-    }
-}
--- src/IG.NexFlow.Core/Dominio/Entidades/EntidadeBase.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace IG.NexFlow.Core.Dominio.Entidades
-{
-    public abstract class EntidadeBase
-    {
-        public virtual long Id { get; protected set; }
-    }
-}
