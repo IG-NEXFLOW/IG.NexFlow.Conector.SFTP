d9d3ba67658c9059ad59d8cf263673c733c7dd65
Igor Daflon <igordafloncouto@gmail.com>
2025-05-16 23:28:15 -0300

    Adiciona entidade ServidorSftp e configurações EF Core
    Inclui referências ao Entity Framework Core no projeto.
    Cria a classe `ServidorSftp` com propriedades essenciais.
    Implementa a configuração da entidade `ServidorSftp` com chaves primárias e tipos de coluna no banco de dados.

---
+++ src/IG.NexFlow.Conector.SFTP.Dominio/Entidades/ServidorSftp.cs
@@ -0,0 +1,13 @@
+﻿namespace IG.NexFlow.Conector.SFTP.Dominio.Entidades
+{
+    public class ServidorSftp
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Host { get; set; } = string.Empty;
+        public int Porta { get; set; }
+        public string Usuario { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public string SenhaSalt { get; set; } = string.Empty;
+    }
+}
---
+++ src/IG.NexFlow.Conector.SFTP.Infra/Mapeamentos/ServidorSftpMap.cs
@@ -0,0 +1,37 @@
+﻿using IG.NexFlow.Conector.SFTP.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IG.NexFlow.Conector.SFTP.Infra.Mapeamentos
+{
+    public class ServidorSftpMap : IEntityTypeConfiguration<ServidorSftp>
+    {
+        public void Configure(EntityTypeBuilder<ServidorSftp> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(255)");
+
+            builder.Property(p => p.Host)
+                .IsRequired()
+                .HasColumnType("varchar(255)");
+
+            builder.Property(p => p.Porta)
+                .IsRequired();
+
+            builder.Property(p => p.Usuario)
+                .IsRequired()
+                .HasColumnType("varchar(255)");
+
+            builder.Property(p => p.Senha)
+                .IsRequired()
+                .HasColumnType("varchar(255)");
+
+            builder.Property(p => p.SenhaSalt)
+                .IsRequired()
+                .HasColumnType("varchar(255)");
+        }
+    }
+}
